e5563d5c60d9df33a401668eba74d84052db16b1
At0ned <sethaholt@gmail.com>
2024-07-01 10:05:00 -0500

    add project

---
+++ Itinerary-Designer/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Itinerary_Designer.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
