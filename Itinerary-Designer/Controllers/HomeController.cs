ac7c39c852ff465ade5d292c1afa5f4a592e4ffb
Ricky <ricky@luiss-air.mynetworksettings.com>
2024-07-02 15:22:43 -0500

    Fixed a few bugs

--- Itinerary-Designer/Controllers/HomeController.cs
+++ Itinerary-Designer/Controllers/HomeController.cs
@@ -1,7 +1,7 @@
 using System.Diagnostics;
-using Microsoft.AspNetCore.Mvc;
-using Itinerary_Designer.Models;
 using System.Linq.Expressions;
+using Itinerary_Designer.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Itinerary_Designer.Controllers;
 
@@ -29,16 +29,23 @@ public class HomeController : Controller
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        return View(
+            new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier }
+        );
     }
+
     //Action for the Currency Conversion
     [HttpPost]
-    public async Task<IActionResult> CovertCurrency(string fromCurrency, string toCurrency, double amount)
+    public async Task<IActionResult> CovertCurrency(
+        string fromCurrency,
+        string toCurrency,
+        double amount
+    )
     {
         try
         {
             ConvertRequest request = new ConvertRequest(fromCurrency, toCurrency, amount);
-            ConveryResponse response = await _exchangeRatesApi.ConvertAsync(request);
+            ConvertResponse response = await _exchangeRatesApi.ConvertAsync(request);
 
             //Preparing data to pass to the View
             ViewData["Amount"] = amount;
@@ -47,13 +54,11 @@ public class HomeController : Controller
             ViewData["ConvertedAmount"] = response.Result;
 
             return View("Index");
-        {
-            catch (Exception ex)
-            {
-                ViewData["Error"] = ex.Message;
-                return View("Index")
-            }
         }
+        catch (Exception ex)
+        {
+            ViewData["Error"] = ex.Message;
+            return View("Index");
         }
     }
 }
