57cfe48bd49d605579c06727799bef353dd18053
caitlinrsmith <catie12smith@gmail.com>
2024-07-02 14:29:28 -0500

    attempting to connect data to the checkboxes

---
+++ Itinerary-Designer/ViewModels/CheckOptionViewModel.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+
+namespace CheckOptionViewModel
+// CheckOption ??
+{
+    public class CheckOption
+    // CheckOptionViewModel ??
+    {
+        public List<CheckOption> OptionList { get; set; }
+        public List<int> SelectedOptionIds { get; set; }
+    }
+}
--- Itinerary-Designer/ViewModels/CreateItineraryViewModel.cs
+++ Itinerary-Designer/ViewModels/CreateItineraryViewModel.cs
@@ -2,5 +2,11 @@ namespace Itinerary_Designer.ViewModels;
 
 public class CreateItineraryViewModel
 {
-public bool IsChecked {get;set;}
+    public bool IsChecked { get; set; }
+    public List<Option> Options { get; set; }
+    public List<int> SelectedOptionIds { get; set; }
+}
+
+public class Option
+{
 }
