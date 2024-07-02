89c9aa6fea43164441fd2e87d66ad8b61451ceb6
kykruz <k.kruzan@yahoo.com>
2024-07-02 12:16:37 -0500

    LocationData.cs updated

--- Itinerary-Designer/Models/LocationData.cs
+++ Itinerary-Designer/Models/LocationData.cs
@@ -7,12 +7,21 @@ public class LocationData
     public int Id { get; set; }
     public string Name { get; set; }
     public string Address { get; set; }
+    public string Category { get; set; }
+    public double PricePerPerson {get; set; }
+    public string Description { get; set; }
+    public string Phone { get; set; }
     
-    public LocationData(int id, string name, string address)
+    public LocationData(int id, string name, string address, string category, double pricePerPerson, string description, string phone)
     {
         Id = id;
         Name = name;
         Address = address;
+        Category = category;
+        PricePerPerson = pricePerPerson;
+        Description = description;
+        Phone = phone;
     }
+
 
 }
