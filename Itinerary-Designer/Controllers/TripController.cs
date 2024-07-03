91e6d2db5bc1e5b101bfc90dbb765bf31083c891
caitlinrsmith <catie12smith@gmail.com>
2024-07-03 15:48:45 -0500

    trying to redirect to a success page after hitting submit

--- Itinerary-Designer/Controllers/TripController.cs
+++ Itinerary-Designer/Controllers/TripController.cs
@@ -1,43 +1,60 @@
 using System.Diagnostics;
-using Microsoft.AspNetCore.Mvc;
-using Trips.Models;
-using Trips.Data;
 using Itinerary.Models;
+using Microsoft.AspNetCore.Mvc;
+using Trips.Data;
+using Trips.Models;
 
 namespace Trips.Controllers
 {
-
-public class TripController : Controller
-{
-    private TripDbContext context;
-
-    public TripController(TripDbContext dbContext)
+    public class TripController : Controller
     {
-        context = dbContext;
-    }
+        private TripDbContext context;
 
-    public IActionResult Index()
-    {
-        List<Trip> Itinerary = context.Itineraries.ToList();
-        return View(Itinerary);
-    }
+        public TripController(TripDbContext dbContext)
+        {
+            context = dbContext;
+        }
 
-    //
-    [HttpGet]
-    [Route("Itinerary/Create")]
-    public IActionResult Create()
-    {
-        List<LocationData> locationDatas = context.LocationDatas.ToList();
-        return View(locationDatas);
-    }
+        public IActionResult Index()
+        {
+            List<Trip> Itinerary = context.Itineraries.ToList();
+            return View(Itinerary);
+        }
 
-    [HttpPost]
-    public IActionResult Delete()
-    {
-        // ViewBag.events = EventData.GetAll();
+        //
+        [HttpGet]
+        [Route("Itinerary/Create")]
+        public IActionResult Create()
+        {
+            List<LocationData> locationDatas = context.LocationDatas.ToList();
+            return View(locationDatas);
+        }
 
-        // gotta come back later and make sure this really directs somewhere
-        return Redirect("/");
+        [HttpPost]
+        public IActionResult Create(List<int> selectedLocations)
+        {
+            if (selectedLocations != null && selectedLocations.Any())
+            {
+                // Process the selected locations
+                // For example, save them to the database or perform other actions
+            }
+            return RedirectToAction("Success");
+        }
+
+    
+        public IActionResult Success()
+        {
+            return View();
+        }
+
+        
+        [HttpPost]
+        public IActionResult Delete()
+        {
+            // ViewBag.events = EventData.GetAll();
+
+            // gotta come back later and make sure this really directs somewhere
+            return Redirect("/");
+        }
     }
-}
 }
