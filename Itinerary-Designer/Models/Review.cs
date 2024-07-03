30ab548e288930a5bc16d26cd843f3645bdbf782
Ricky <ricky@luiss-air.mynetworksettings.com>
2024-07-03 13:28:54 -0500

    Fixing Review

---
+++ Itinerary-Designer/Models/Review.cs
@@ -0,0 +1,19 @@
+
+namespace Review.Models;
+
+public class Review
+{
+    public int Id { get; set; }
+    private int nextId { get; set; }
+    public string Title { get; set; }
+    public string Content { get; set; }
+    public DateTime PostedDate { get; set; }
+    public int Rating { get; set; }
+    // public bool IsAproved { get; set; }
+    // public bool IsDeleted { get; set; }
+
+    public Review()
+    {
+        PostedDate = DateTime.UtcNow;
+    }
+}
