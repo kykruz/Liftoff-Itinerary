51582bf0e746ed87eb91a105a4a30199ad0af115
Ricky <ricky@luiss-air.mynetworksettings.com>
2024-07-05 16:47:57 -0500

    TripDbContext Adjustment

--- Itinerary-Designer/Data/TripDbContext.cs
+++ Itinerary-Designer/Data/TripDbContext.cs
@@ -1,22 +1,33 @@
 using System;
-using Microsoft.EntityFrameworkCore;
-using Trips.Models;
-using Microsoft.Extensions.Hosting;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Microsoft.AspNetCore.Identity;
 using Itinerary.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Hosting;
 using Reviews.Models;
-
+using Trips.Models;
+using static Reviews.Models.Review;
 
 namespace Trips.Data
 {
-    public class TripDbContext : IdentityDbContext <IdentityUser, IdentityRole, string>
+    public class TripDbContext : IdentityDbContext<IdentityUser, IdentityRole, string>
     {
-        public DbSet<Trip> Itineraries {get; set;}
-        public DbSet<LocationData> LocationDatas {get; set;}
+        public DbSet<Trip> Itineraries { get; set; }
+        public DbSet<LocationData> LocationDatas { get; set; }
         public DbSet<Review> Reviews { get; set; }
-          public TripDbContext(DbContextOptions<TripDbContext> options) : base(options)
-        {
-        }
+        public DbSet<Comment> Comments { get; set; }
+
+        public TripDbContext(DbContextOptions<TripDbContext> options)
+            : base(options) { }
+        //     protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //     {
+        //         base.OnModelCreating(modelBuilder);
+
+        //         modelBuilder.Entity<Comment>()
+        //             .HasOne(c => c.Review)
+        //             .WithMany(r => r.Comments)
+        //             .HasForeignKey(c => c.ReviewId)
+        //             .OnDelete(DeleteBehavior.Cascade);
+        // }
     }
 }
