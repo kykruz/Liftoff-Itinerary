d477ec45cec06bcde522949e2d218e6700c71c45
At0ned <sethaholt@gmail.com>
2024-07-07 10:32:36 -0500

    merge

---
+++ Itinerary-Designer/Models/Itinerary.cs
@@ -0,0 +1,38 @@
+using System;
+using Trips.Models;
+
+namespace Trips.Models
+{
+    public class Itinerary
+    {
+        public int? Id { get; set; }
+        public string Name { get; set; }
+        public List<int> LocationIds {get; set;}
+        public DateTime Date {get; set;}
+
+      
+        public Itinerary() {}
+        public Itinerary (string name)
+        {
+            Name = name;
+            
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is LocationData @location && Id == @location.Id;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id);
+        }
+    }
+
+   
+}
