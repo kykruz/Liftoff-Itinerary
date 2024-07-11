f12a6f2e5984633f41e5f9b9078d9212d6b29083
caitlinrsmith <catie12smith@gmail.com>
2024-07-10 22:44:33 -0500

    category filtering checkboxes working

---
+++ Itinerary-Designer/ViewModels/LocationViewModel.cs
@@ -0,0 +1,9 @@
+using System;
+using System.Collections.Generic;
+using Trips.Models;
+public class LocationViewModel
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Category { get; set; }
+}
