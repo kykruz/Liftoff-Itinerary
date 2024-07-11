7dd22a5fcb671e7ec57304dbab8b1d1976e0a928
caitlinrsmith <catie12smith@gmail.com>
2024-07-11 11:04:30 -0500

    added the checkbox form all categories and it is preselected on load

--- Itinerary-Designer/ViewModels/CreateItineraryViewModel.cs
+++ Itinerary-Designer/ViewModels/CreateItineraryViewModel.cs
@@ -1,36 +1,30 @@
-
-using System;
+﻿using System;
 using System.Collections.Generic;
 using Trips.Models;
-using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-
 
 public class CreateItineraryViewModel
 {
     [Required(ErrorMessage = "Name is required")]
     public string Name { get; set; }
 
-    public DateTime Date { get; set; }
-    public List<LocationData> AvailableLocations { get; set; }
-    public List<int> SelectedLocationIds { get; set; }
-    public List<string> AvailableCategories { get; set; }
-    public List<string> SelectedCategories { get; set; }
-    public int NumberOfPeople { get; set; }
-    public decimal TotalCost { get; set; }
-    public decimal TotalCostForAllPeople { get; set; }
-
-    
-    [Required(ErrorMessage = "Please select at least one location")]
-    public List<int> SelectedLocationIds { get; set; } 
-    
     [Required(ErrorMessage = "Date is required")]
-    public DateTime Date {get; set;}
-    
-    public List<LocationData> AvailableLocations { get; set; } 
-    
+    public DateTime Date { get; set; }
 
+    [Required(ErrorMessage = "Please select at least one location")]
+    public List<int> SelectedLocationIds { get; set; }
+
+    public List<LocationData> AvailableLocations { get; set; }
+
+    public List<string> AvailableCategories { get; set; }
+
+    public List<string> SelectedCategories { get; set; }
+
+    public int NumberOfPeople { get; set; }
+
+    public decimal TotalCost { get; set; }
+
+    public decimal TotalCostForAllPeople { get; set; }
 
     public CreateItineraryViewModel()
     {
